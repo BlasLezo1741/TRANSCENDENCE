534d1406c166b9f4c26a705b7ededc8becdc256a
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2026-03-22 13:32:31 +0100

    chore: Makefiles fclean cleans better docker ecosystem

--- clean_env.sh
+++ clean_env.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
+echo "stoping containers"
+docker stop $(docker ps -q)
+docker rm $(docker ps -aq)
 echo "Removing data from persistent volumes"
 docker run --rm -v /home/luicasad/data/grafana:/target alpine sh -c 'rm -rf /target/* /target/.[!.]* 2>/dev/null; echo done'
 docker run --rm -v /home/luicasad/data/dbserver:/target alpine sh -c 'rm -rf /target/* /target/.[!.]* 2>/dev/null; echo done'
 rmdir /home/luicasad/data/grafana
 rmdir /home/luicasad/data/dbserver
+echo "Removing named volumes"
+docker volume rm $(docker volume ls -q)
+
 echo "Cleaning docker system"
 yes | docker system prune -a --volumes > /dev/null 2>&1
 echo "Cleaning npm environment's packages"
