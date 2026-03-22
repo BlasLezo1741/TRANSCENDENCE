5abc7603770c2d2de6e0a0b693276d681294fa2e
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2026-03-22 13:28:01 +0100

    chore: delivery script first removes previous content of srcs/ and docs/

--- entrega.sh
+++ entrega.sh
@@ -2,6 +2,8 @@
 cp Makefile ~/Desktop/entrega
 cp README.md ~/Desktop/entrega
 cp .gitignore ~/Desktop/entrega
+rm -rf ~/Desktop/entrega/srcs 
+rm -rf ~/Desktop/entrega/docs
 cp -r srcs ~/Desktop/entrega
 cp -r docs ~/Desktop/entrega
 rm ~/Desktop/entrega/srcs/.env
